77f0c2dba3668a994449adb411880b33e6315e75
Max Dumas <maltor124@gmail.com>
2021-08-24 12:51:03 -0400

    Memoize token list loader

--- src/client.ts
+++ src/client.ts
@@ -2,23 +2,15 @@ import assert from 'node:assert';
 import fs from 'node:fs/promises';
 import url from 'node:url';
 
-import ethers from 'ethers';
 import ethscan from '@mycrypto/eth-scan';
+import ethers from 'ethers';
+import mem from 'mem';
 
 import { LunchMoneyCryptoConnectionBalances } from './types.js';
 
 /** The minimum balance (in wei) that a token should have in order to be
  * considered for returning as a balance. */
 const NEGLIGIBLE_BALANCE_THRESHOLD = 1000;
-
-interface EthereumWalletApiConfig {
-  etherscan: string;
-}
-
-interface EthereumWalletClientConfig {
-  api: EthereumWalletApiConfig;
-  negligibleBalanceThreshold?: string;
-}
 
 interface Token {
   address: string;
@@ -29,13 +21,13 @@ interface Token {
   logoURI: string;
 }
 
-export const loadTokenList = async (): Promise<Token[]> => {
+export const loadTokenList = mem(async (): Promise<Token[]> => {
   assert(import.meta.resolve);
 
   const tokenListPath = url.fileURLToPath(await import.meta.resolve('../fixtures/1inch.json'));
   const tokenList = JSON.parse((await fs.readFile(tokenListPath)).toString('utf-8'));
   return tokenList.tokens;
-};
+});
 
 export const loadTokenBalances = async (
   walletAddress: string,
@@ -48,12 +40,12 @@ export const loadTokenBalances = async (
   const map = await ethscan.getTokensBalance(
     provider,
     walletAddress,
-    tokenList.map((t: any) => t.address),
+    tokenList.map((t) => t.address),
   );
 
   const balances = Object.entries(map).flatMap(
     ([tokenAddress, tokenBalance]: [string, bigint]) => {
-      const token = tokenList.find((t: any) => t.address === tokenAddress);
+      const token = tokenList.find((t) => t.address === tokenAddress);
       assert(token);
 
       if (tokenBalance > NEGLIGIBLE_BALANCE_THRESHOLD) {
