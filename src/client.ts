f0595e8a9cab8e0ebe1c37341e9270b8500cb3c4
Max Dumas <maltor124@gmail.com>
2021-08-24 13:16:16 -0400

    Fold client code into main module

--- src/client.ts
+++ src/client.ts
@@ -2,15 +2,10 @@ import assert from 'node:assert';
 import fs from 'node:fs/promises';
 import url from 'node:url';
 
-import ethscan from '@mycrypto/eth-scan';
 import ethers, { BigNumberish } from 'ethers';
 import mem from 'mem';
 
-import { CryptoBalance, LunchMoneyCryptoConnectionBalances } from './types.js';
-
-/** The minimum balance (in wei) that a token should have in order to be
- * considered for returning as a balance. */
-const NEGLIGIBLE_BALANCE_THRESHOLD = 1000;
+import { CryptoBalance } from './types.js';
 
 interface Token {
   address: string;
@@ -29,37 +24,7 @@ export const loadTokenList = mem(async (): Promise<Token[]> => {
   return tokenList.tokens;
 });
 
-const weiToEth = ({ asset, amount }: { asset: string; amount: BigNumberish }): CryptoBalance => ({
+export const weiToEth = ({ asset, amount }: { asset: string; amount: BigNumberish }): CryptoBalance => ({
   asset,
   amount: ethers.utils.formatEther(amount),
 });
-
-export const loadTokenBalances = async (
-  walletAddress: string,
-  tokenList: Token[],
-  provider: ethers.providers.BaseProvider,
-): Promise<LunchMoneyCryptoConnectionBalances> => {
-  const weiBalance = await provider.getBalance(walletAddress);
-
-  const map = await ethscan.getTokensBalance(
-    provider,
-    walletAddress,
-    tokenList.map((t) => t.address),
-  );
-
-  const balances = Object.entries(map).flatMap(([tokenAddress, amount]) => {
-    const token = tokenList.find((t) => t.address === tokenAddress);
-    assert(token);
-
-    if (amount > NEGLIGIBLE_BALANCE_THRESHOLD) {
-      return [{ asset: token.symbol, amount }];
-    } else {
-      return [];
-    }
-  });
-
-  return {
-    providerName: 'wallet_ethereum',
-    balances: [{ asset: 'ETH', amount: weiBalance }, ...balances].map(weiToEth),
-  };
-};
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
+import assert from 'node:assert';
+
 import {
   LunchMoneyCryptoConnection,
   LunchMoneyCryptoConnectionContext,
   LunchMoneyCryptoConnectionConfig,
 } from './types.js';
 
+import ethscan from '@mycrypto/eth-scan';
 import type ethers from 'ethers';
 
-import { loadTokenBalances, loadTokenList } from './client.js';
+import { loadTokenList, weiToEth } from './client.js';
 
 export { LunchMoneyCryptoConnection } from './types.js';
+
+/** The minimum balance (in wei) that a token should have in order to be
+ * considered for returning as a balance. */
+const NEGLIGIBLE_BALANCE_THRESHOLD = 1000;
 
 interface LunchMoneyEthereumWalletConnectionConfig extends LunchMoneyCryptoConnectionConfig {
   /** The unique ID of the user's wallet address on the blockchain. */
@@ -29,6 +36,28 @@ export const LunchMoneyEthereumWalletConnection: LunchMoneyCryptoConnection<
   },
   async getBalances(config, context) {
     const tokenList = await loadTokenList();
-    return loadTokenBalances(config.walletAddress, tokenList, context.provider);
+    const weiBalance = await context.provider.getBalance(config.walletAddress);
+
+    const map = await ethscan.getTokensBalance(
+      context.provider,
+      config.walletAddress,
+      tokenList.map((t) => t.address),
+    );
+
+    const balances = Object.entries(map).flatMap(([tokenAddress, amount]) => {
+      const token = tokenList.find((t) => t.address === tokenAddress);
+      assert(token);
+
+      if (amount > (config.negligibleBalanceThreshold ?? NEGLIGIBLE_BALANCE_THRESHOLD)) {
+        return [{ asset: token.symbol, amount }];
+      } else {
+        return [];
+      }
+    });
+
+    return {
+      providerName: 'wallet_ethereum',
+      balances: [{ asset: 'ETH', amount: weiBalance }, ...balances].map(weiToEth),
+    };
   },
 };
