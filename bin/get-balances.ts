5179f4954f16fa81bf1f294c0a9c425a195bfb72
Max Dumas <max@dumas.nyc>
2021-12-08 15:39:25 -0500

    Move code to main file

--- bin/get-balances.ts
+++ bin/get-balances.ts
@@ -1,16 +1,8 @@
 #!/usr/bin/env node --experimental-json-modules --loader ts-node/esm
 
 import assert from 'node:assert';
-import { encode } from 'node:querystring';
-import EventSource from 'eventsource';
 
-import { CryptoBalance } from '../src/types.js';
-
-const ZAPPER_FI_API_URL = 'https://api.zapper.fi/v1/balances';
-// This API key is public and shared with all users. This API is publicly
-// available, free of charge. See here for details:
-// https://docs.zapper.fi/zapper-api/endpoints
-const ZAPPER_FI_API_KEY = '96e0cc51-a62e-42ca-acee-910ea7d2a241';
+import { LunchMoneyEthereumWalletConnection } from '../src/main.js';
 
 const requireEnv = (key: string): string => {
   const value = process.env[key];
@@ -19,42 +11,14 @@ const requireEnv = (key: string): string => {
 };
 
 const ETH_ADDRESS = requireEnv('ETH_ADDRESS');
-const ETH_ADDRESS_INDEX = ETH_ADDRESS.toLowerCase();
 
-const qs = encode({
-  'addresses[]': ETH_ADDRESS,
-  api_key: ZAPPER_FI_API_KEY,
-});
-
-const url = ZAPPER_FI_API_URL + '?' + qs;
-
-const es = new EventSource(url);
-
-const hasAnyNonZeroBalances = (balanceData: any) => balanceData.balances[ETH_ADDRESS_INDEX].products.length > 0;
-
-let allBalances: CryptoBalance[] = [];
-
-es.addEventListener('balance', (ev) => {
-  const data = JSON.parse(ev.data);
-  if (hasAnyNonZeroBalances(data)) {
-    const p = data.balances[ETH_ADDRESS_INDEX].products;
-
-    const balances: CryptoBalance[] = p.flatMap((p: any) =>
-      p.assets.flatMap((a: any) =>
-        a.tokens.map((t: any) => ({
-          asset: t.symbol,
-          amount: t.balance.toString(),
-          amountInUSD: t.balanceUSD.toString(),
-        })),
-      ),
-    );
-
-    allBalances = allBalances.concat(balances);
-  }
-});
-
-es.addEventListener('end', () => {
-  console.log('Finished stream. Closing connection.');
-  console.log(allBalances.sort((a, b) => parseFloat(a.amountInUSD) - parseFloat(b.amountInUSD)));
-  es.close();
-});
+(async function () {
+  console.log(
+    await LunchMoneyEthereumWalletConnection.getBalances(
+      {
+        walletAddress: ETH_ADDRESS,
+      },
+      {},
+    ),
+  );
+})();
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,20 @@ import {
   LunchMoneyCryptoConnection,
   LunchMoneyCryptoConnectionContext,
   LunchMoneyCryptoConnectionConfig,
+  CryptoBalance,
 } from './types.js';
 
-import { loadTokenList, EthereumWalletClient } from './client.js';
-import * as ethers from 'ethers';
+import { encode } from 'node:querystring';
+import EventSource from 'eventsource';
 
 export { LunchMoneyCryptoConnection } from './types.js';
-export { createEthereumWalletClient, EthereumWalletClient } from './client.js';
+// export { createEthereumWalletClient, EthereumWalletClient } from './client.js';
+
+const ZAPPER_FI_API_URL = 'https://api.zapper.fi/v1/balances';
+// This API key is public and shared with all users. This API is publicly
+// available, free of charge. See here for details:
+// https://docs.zapper.fi/zapper-api/endpoints
+const ZAPPER_FI_API_KEY = '96e0cc51-a62e-42ca-acee-910ea7d2a241';
 
 /** The minimum balance (in wei) that a token should have in order to be
  * considered for returning as a balance. */
@@ -20,9 +27,7 @@ interface LunchMoneyEthereumWalletConnectionConfig extends LunchMoneyCryptoConne
   negligibleBalanceThreshold?: number;
 }
 
-interface LunchMoneyEthereumWalletConnectionContext extends LunchMoneyCryptoConnectionContext {
-  client: EthereumWalletClient;
-}
+type LunchMoneyEthereumWalletConnectionContext = LunchMoneyCryptoConnectionContext;
 
 export const LunchMoneyEthereumWalletConnection: LunchMoneyCryptoConnection<
   LunchMoneyEthereumWalletConnectionConfig,
@@ -31,28 +36,54 @@ export const LunchMoneyEthereumWalletConnection: LunchMoneyCryptoConnection<
   async initiate(config, context) {
     return this.getBalances(config, context);
   },
-  async getBalances({ walletAddress, negligibleBalanceThreshold = NEGLIGIBLE_BALANCE_THRESHOLD }, { client }) {
-    const weiBalance = await client.getWeiBalance(walletAddress);
+  async getBalances({ walletAddress, negligibleBalanceThreshold = NEGLIGIBLE_BALANCE_THRESHOLD }, {}) {
+    // For some reason the address that is returned in the response is
+    // lowercased.
+    const walletAddressIndex = walletAddress.toLowerCase();
 
-    const tokenList = await loadTokenList();
-    const map = await client.getTokensBalance(
-      walletAddress,
-      tokenList.map((t) => t.address),
-    );
+    const qs = encode({
+      'addresses[]': walletAddress,
+      api_key: ZAPPER_FI_API_KEY,
+    });
 
-    const balances = tokenList
-      .map((t) => ({
-        asset: t.symbol,
-        amount: map[t.address] ?? 0,
-      }))
-      .concat({ asset: 'ETH', amount: weiBalance })
-      .filter((b) => b.amount > negligibleBalanceThreshold)
-      .map(({ asset, amount }) => ({ asset, amount: ethers.utils.formatEther(amount) }))
-      .sort((a, b) => a.asset.localeCompare(b.asset));
+    const es = new EventSource(ZAPPER_FI_API_URL + '?' + qs);
 
-    return {
-      providerName: 'wallet_ethereum',
-      balances,
-    };
+    const hasAnyNonZeroBalances = (balanceData: any) => balanceData.balances[walletAddressIndex].products.length > 0;
+
+    let balances: CryptoBalance[] = [];
+
+    es.addEventListener('balance', (ev) => {
+      const data = JSON.parse(ev.data);
+      if (hasAnyNonZeroBalances(data)) {
+        const p = data.balances[walletAddressIndex].products;
+
+        const appBalances: CryptoBalance[] = p.flatMap((p: any) =>
+          p.assets.flatMap((a: any) =>
+            a.tokens.map((t: any) => ({
+              asset: t.symbol,
+              amount: t.balance.toString(),
+              amountInUSD: t.balanceUSD.toString(),
+            })),
+          ),
+        );
+
+        balances = balances.concat(appBalances);
+      }
+    });
+
+    return new Promise((resolve, reject) => {
+      es.addEventListener('end', () => {
+        es.close();
+        resolve({
+          providerName: 'wallet_ethereum',
+          balances,
+        });
+      });
+
+      es.addEventListener('error', (ev) => {
+        es.close();
+        reject(ev);
+      });
+    });
   },
 };
